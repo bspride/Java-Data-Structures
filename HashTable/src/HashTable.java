b581d1f850c495ec19871c4310ad2d81b7814775
Michael Clarke <bspride11@gmail.com>
2016-11-17 23:22:15 -0500

    Implement add, exists, get, remove, and find methods

--- HashTable/src/HashTable.java
+++ HashTable/src/HashTable.java
@@ -7,7 +7,7 @@
  *
  */
 public class HashTable<T1, T2> implements IHashTable<T1, T2> {
-	private IHashItem[] data;
+	private IHashItem<T1, T2>[] data;
 	private int capacity;
 	private int size;
 	
@@ -15,7 +15,74 @@ public class HashTable<T1, T2> implements IHashTable<T1, T2> {
 		data = new HashItem[capacity];
 		size = 0;
 	}
+
+	@Override
+	public void add(T1 key, T2 value) {
+		HashItem<T1, T2> insert = new HashItem<T1, T2>(key, value);
+		
+		if(this.size >= this.capacity/2) {
+			this.grow();
+		}
+		
+		int index = this.hash(key);
+		while(this.data[index] != null || !this.data[index].isDeleted()) {
+			index = (index+1)%this.capacity;
+		}
+		
+		this.data[index] = insert;
+		this.size++;
+	}
+
+	@Override
+	public boolean exists(T1 key) {
+		int index = this.find(key);
+		if(index != -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public T2 get(T1 key) {
+		int index = this.find(key);
+		if(index != -1) {
+			return this.data[index].getValue();
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void remove(T1 key) {
+		int index = this.find(key);
+		if(index != -1) {
+			this.data[index].setDeleted();
+		}
+		
+		this.size--;
+		if(this.size <= this.capacity/4) {
+			this.shrink();
+		}
+	}
 	
+	private int find(T1 key) {
+		int index = this.hash(key);
+		int originalIndex = index;
+		
+		while(this.data[index] != null) {
+			if(!this.data[index].isDeleted() && this.data[index].getKey().equals(key)) {
+				return index;
+			}
+			index++;
+			if(index == originalIndex) {
+				break;
+			}
+		}
+		
+		return -1;
+	}
+
 	private int hash(T1 key) {
 		return key.hashCode();
 	}
@@ -25,29 +92,6 @@ public class HashTable<T1, T2> implements IHashTable<T1, T2> {
 	}
 	
 	private void grow() {
-		
-	}
-
-	@Override
-	public void add(T1 key, T2 value) {
-		
-	}
-
-	@Override
-	public boolean exists(T1 key) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public T2 get(T1 key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void remove(T1 key) {
-		// TODO Auto-generated method stub
 		
 	}
 
